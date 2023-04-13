b86971c61e004ad9efee08393eed34f6f137bc8d
Jeremy Berry <69398054+twobit-five@users.noreply.github.com>
2023-04-12 20:10:01 -0500

    Update Esp32-Device.ino
    Fixed issue after disconnection, where the device stopped advertising.  
    
    Added TX Power characteristic.
    
    Removed battery level calculations (Using random values for now)

--- Esp32-Device.ino
+++ Esp32-Device.ino
@@ -4,56 +4,78 @@
 #include <BLE2902.h>
 #include <time.h>
 #include <stdlib.h>
-#include <driver/adc.h> // Include the required header for the ADC
 #include <BLEAdvertisedDevice.h>
 
 #define BATTERY_LEVEL_SERVICE_UUID        "180F"
 #define BATTERY_LEVEL_CHARACTERISTIC_UUID "2A19"
 
+#define TX_POWER_SERVICE_UUID "1804"
+#define TX_POWER_LEVEL_CHARACTERISTIC_UUID "2A07"
+
 #define BAUD_RATE 115200
 
-//device name should change and be unique for each device
-#define DEVICE_NAME "SphereLink-5467"
-
-// Define the ADC pin and conversion factor
-#define BATTERY_PIN 34
-#define VOLTAGE_DIVIDER_RATIO 2.0 // Adjust this based on your voltage divider
+//device name should change and be unique for each device. 
+//TODO Modify to be Uniquilly generated at compile time?
+#define DEVICE_NAME "SphereLink-1357"
 
 bool deviceConnected = false;
 
 unsigned long previousBatteryUpdateTime = 0;
-unsigned long batteryUpdateInterval = 600000; // 10 minutes in milliseconds
+unsigned long batteryUpdateInterval = 10000; // 1 minutes in milliseconds
 
 
 // class used to easily verify whether the device is connected.
 class ServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
       deviceConnected = true;
+      Serial.println("onConnect Event Received.");
     };
 
     void onDisconnect(BLEServer* pServer) {
       deviceConnected = false;
+      Serial.println("oNDisconnect Event Received.");
+      //Start Advertising Again
+      BLEDevice::startAdvertising();
     }
+
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string value = pCharacteristic->getValue();
+
+        Serial.println("onWrite was Called");
+    }
+
+    void onNotify(BLECharacteristic* pCharacteristic) {
+      std::string value = pCharacteristic->getValue();
+      
+        Serial.println("onNotify was Called");
+        Serial.print("New value: ");
+        //Serial.println(value);
+    }
+
+   void onRead(BLECharacteristic* pCharacteristic) {
+
+      Serial.println("onRead was called");
+   }
 };
 
 uint8_t batteryLevel = 100;
 BLECharacteristic *pBatteryCharacteristic;
+uint8_t txPowerLevel = 12; // Use the same value as the one you set for advertising
+BLECharacteristic *pTxPowerCharacteristic;
 
 void setup() {
   
   Serial.begin(BAUD_RATE);
-  Serial.println("Starting BLE work!");
-
-  // Configure the ADC
-  adc1_config_width(ADC_WIDTH_BIT_12);
-  adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_11);
+  Serial.println("Starting SphereLink ESP-32 Device!");
 
   BLEDevice::init(DEVICE_NAME);
+  
   BLEServer *pServer = BLEDevice::createServer();
   pServer->setCallbacks(new ServerCallbacks());  // Set the function that handles Server Callbacks
+  
+  
   BLEService *pService = pServer->createService(BATTERY_LEVEL_SERVICE_UUID);
-
-  //Create the battery Characteristic
+  //Create the battery service and Characteristic
   pBatteryCharacteristic = pService->createCharacteristic(
                                          BATTERY_LEVEL_CHARACTERISTIC_UUID,
                                          BLECharacteristic::PROPERTY_READ |
@@ -62,25 +84,38 @@ void setup() {
 
   pBatteryCharacteristic->addDescriptor(new BLE2902());
   pBatteryCharacteristic->setValue(&batteryLevel, 1);
-
   // Add the Characteristic User Description descriptor
   BLEDescriptor *pUserDescriptionDescriptor = new BLEDescriptor(BLEUUID((uint16_t)0x2901));
   pUserDescriptionDescriptor->setValue("Percentage 0 - 100");
   pBatteryCharacteristic->addDescriptor(pUserDescriptionDescriptor);
 
-  //Add additional characteristics
+  //TODO Fix, currently not working.  
+  //Also May not be needed... Check into.
+  // Create the TX Power service and characteristic
+  BLEService *pTxPowerService = pServer->createService(TX_POWER_SERVICE_UUID);
+  pTxPowerCharacteristic = pTxPowerService->createCharacteristic(
+      TX_POWER_LEVEL_CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ);
+  // Set the initial value of the TX Power Level characteristic
+  pTxPowerCharacteristic->setValue(&txPowerLevel, 1);
+
 
   pService->start();
+
 
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->setAppearance(64);
   pAdvertising->addServiceUUID(BATTERY_LEVEL_SERVICE_UUID);
-  pAdvertising->setScanResponse(true);
+  pAdvertising->addServiceUUID(TX_POWER_SERVICE_UUID); 
+  pAdvertising->setScanResponse(false);
   pAdvertising->setMinPreferred(0x06); // Minimum connection interval: 7.5 ms
   pAdvertising->setMaxPreferred(0x12); // Maximum connection interval: 18 ms
+  
+  //TODO need to find way to set the TXpower, so we can match the value on the TX Service.
+  //pAdvertising-> setPower(12);  
   BLEDevice::startAdvertising();
 
-  Serial.println("Characteristic and battery level defined! Now you can read them in your phone!");
+  Serial.println("Waiting a client connection to notify...");
 }
 
 void loop() {
@@ -88,33 +123,21 @@ void loop() {
   if (deviceConnected) {
 
     unsigned long currentTime = millis();
+
     if (currentTime - previousBatteryUpdateTime >= batteryUpdateInterval) {
-    previousBatteryUpdateTime = currentTime;
-    batteryLevel = convertVoltageToBatteryPercentage(readBatteryLevel());
-    pBatteryCharacteristic->setValue(&batteryLevel, 1);
-    pBatteryCharacteristic->notify();
-    Serial.print("Battery level: ");
-    Serial.println(batteryLevel);
+
+      previousBatteryUpdateTime = currentTime;
+
+      //batteryLevel = convertVoltageToBatteryPercentage(readBatteryLevel());
+      uint8_t batteryLevel =  random(1, 101);
+      
+      Serial.print("Battery level: ");
+      Serial.println(batteryLevel);
+      
+      pBatteryCharacteristic->setValue(&batteryLevel, 1);
+      pBatteryCharacteristic->notify();
     }
   }
 
   delay(2000);
-}
-
-uint8_t readBatteryLevel() {
-
-    // Read the battery voltage
-    int adc_raw = adc1_get_raw(ADC1_CHANNEL_0);
-    float voltage = ((float)adc_raw / 4095.0) * 3.3 * VOLTAGE_DIVIDER_RATIO;
-}
-
-// This function converts the battery voltage to a percentage based on the battery's discharge curve
-uint8_t convertVoltageToBatteryPercentage(float voltage) {
-
-  // Replace the following lines with the conversion logic specific to your battery's discharge curve
-  float minVoltage = 3.0; // Minimum voltage when the battery is considered empty
-  float maxVoltage = 5.0; // Maximum voltage when the battery is considered full
-
-  uint8_t percentage = (uint8_t)((voltage - minVoltage) / (maxVoltage - minVoltage) * 100);
-  return percentage;
 }
